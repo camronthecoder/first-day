808daaa37343c20f021fab3a0e3b0ced3db86d9b
camronthecoder <106614872+camronthecoder@users.noreply.github.com>
2022-06-09 19:23:06 -0400

    draft

---
+++ firstDay.js
@@ -0,0 +1,9 @@
+
+inputString= "Welcome to 30 Days of code!"
+function processData(inputString) {
+    // This line of code prints the first line of output
+    console.log("Hello, World.");
+    console.log(inputString)
+    // Write the second line of output that prints the contents of 'inputString' here.
+}
+
